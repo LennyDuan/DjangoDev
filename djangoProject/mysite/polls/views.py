5e321b91536f273c8fb26ba8d568695b0b67fe50
段鸿易 <Lenny@Lennys-Mac.local>
2017-07-23 13:38:53 +0100

    django project P2

---
+++ djangoProject/mysite/polls/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(req):
+    return HttpResponse("Hello, Django world! This is first Index view for polls")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
