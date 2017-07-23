bee99408979e9712d451cb6e9017cb6e41d75f77
段鸿易 <Lenny@Lennys-Mac.local>
2017-07-23 14:47:40 +0100

    f3

--- djangoProject/mysite/polls/views.py
+++ djangoProject/mysite/polls/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(req):
-    return HttpResponse("Hello, Django world! This is first Index view for polls")
+
+    latest_question_list = Question.objects.order_by('pub_time')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(req, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
