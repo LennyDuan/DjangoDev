fbdfd243c4c886ba9454b81bc88ee1a5ed207d4c
LennyDuan <hod8@aber.ac.uk>
2017-07-31 11:28:15 +0100

    create index

--- myStudy/mystudy/polls/api/v1/question.py
+++ myStudy/mystudy/polls/api/v1/question.py
@@ -3,3 +3,7 @@ from django.http import Http404, HttpResponse, JsonResponse
 from django.core import serializers
 from polls.models import User, Questionnaire, Question, Study
 import json
+
+# Create Questionnaire & Question API
+def index(request):
+    return HttpResponse("Hello, welcome to question api index page!")
