6353149bc95e69bd9b272fc7cfaaa917fdf7eff6
LennyDuan <hod8@aber.ac.uk>
2017-08-05 17:37:12 +0100

    finish post diary by email

--- myStudy/mystudy/polls/api/v1/post/diaryP.py
+++ myStudy/mystudy/polls/api/v1/post/diaryP.py
@@ -32,3 +32,30 @@ def diaryPost(request, pk_id):
             return HttpResponse("Post data failed: " + str(e))
 
     return HttpResponse("This is the POST api!!")
+
+@csrf_exempt
+def diaryPostUserEmail(request, userInfo_email):
+    if request.method == 'POST':
+        userInfo = get_object_or_404(UserInfo, userInfo_email=userInfo_email)
+        try:
+            received_data = request.body.decode("utf-8")
+            body = json.loads(received_data)
+            diary_id = body["diary_id"]
+            diary_skill = body["diary_skill"]
+            diary_title = body["diary_title"]
+            diary_detail = body["diary_detail"]
+            diary_userInfo = userInfo
+            diary = Diary(
+                diary_id=diary_id,
+                diary_skill=diary_skill,
+                diary_title=diary_title,
+                diary_detail=diary_detail,
+                diary_userInfo=diary_userInfo,
+            )
+            # Question model Save
+            diary.save()
+            return HttpResponse("it was a Diary post request!!__: " + json.dumps(body))
+        except Exception as e:
+            return HttpResponse("Post data failed: " + str(e))
+
+    return HttpResponse("This is the POST api!!")
