37a9f1f8eabef269cb27d3a1e2c4439e5a848b9b
LennyDuan <hod8@aber.ac.uk>
2017-08-10 13:16:09 +0100

    book Code

---
+++ android/Kotlin-for-Android-Developers-master/app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -0,0 +1,42 @@
+package com.antonioleiva.weatherapp.ui.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.antonioleiva.weatherapp.R
+import com.antonioleiva.weatherapp.domain.model.Forecast
+import com.antonioleiva.weatherapp.domain.model.ForecastList
+import com.antonioleiva.weatherapp.extensions.ctx
+import com.antonioleiva.weatherapp.extensions.toDateString
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
+
+class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Forecast) -> Unit) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindForecast(weekForecast[position])
+    }
+
+    override fun getItemCount() = weekForecast.size
+
+    class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date.toDateString()
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
+                itemView.setOnClickListener { itemClick(this) }
+            }
+        }
+    }
+}
