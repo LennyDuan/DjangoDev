f45454469cb3616e5e39d298c97217b6557deea9
LennyDuan <hod8@aber.ac.uk>
2017-08-12 11:51:45 +0100

    study fild spinner

--- Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/ui/AccountActivities.kt
+++ Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/ui/AccountActivities.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.lenny.studyresearchapp.common.OutputUtil.toast
 import kotlinx.android.synthetic.main.activity_account_activities.*
 import java.util.*
@@ -23,6 +26,26 @@ class AccountActivities : AppCompatActivity() {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         dateStartPickerInit(account_start_date)
         dateEndPickerInit(account_end_date)
+        studyFieldSelecterInit()
+    }
+
+    private fun studyFieldSelecterInit() {
+        var studyField : Array<String>? = null
+        studyField = arrayOf("code", "football")
+
+        var arrayAdapter : ArrayAdapter<String> = ArrayAdapter(
+                this, android.R.layout.simple_spinner_item, studyField)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        account_study_spinner.adapter = arrayAdapter
+
+        account_study_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                account_study_text.setText(studyField!![position])
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
     }
 
 
