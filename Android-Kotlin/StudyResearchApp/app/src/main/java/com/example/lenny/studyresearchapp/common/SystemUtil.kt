bfa2903e4c0ad5a39863a87986958482361f787e
LennyDuan <hod8@aber.ac.uk>
2017-08-21 12:57:11 +0100

    finish Diary Location

--- Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/common/SystemUtil.kt
+++ Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/common/SystemUtil.kt
@@ -2,6 +2,11 @@ package com.example.lenny.studyresearchapp.common
 
 import android.content.Context
 import android.provider.Settings.Secure
+import android.location.Criteria
+import android.content.Context.LOCATION_SERVICE
+import android.location.LocationManager
+
+
 
 /**
  * Created by Lenny on 11/08/2017.
