373f528273c30df4134a2567cd62d6af5a7caf5b
LennyDuan <hod8@aber.ac.uk>
2017-08-23 14:25:16 +0100

    finish delete api

--- Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/network/ServiceInterface.kt
+++ Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/network/ServiceInterface.kt
@@ -8,4 +8,5 @@ import org.json.JSONObject
 interface ServiceInterface {
     fun post(path: String, params: JSONObject, completionHandler: (response: JSONObject?) -> Unit)
     fun get(path: String, completionHandler: (response: String?) -> Unit)
+    fun delete(path: String, completionHandler: (response: String?) -> Unit)
 }
