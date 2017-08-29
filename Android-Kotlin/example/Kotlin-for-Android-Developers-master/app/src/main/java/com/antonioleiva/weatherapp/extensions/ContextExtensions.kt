efb636a5f893eaa2d2a4512be2e221463593793c
LennyDuan <hod8@aber.ac.uk>
2017-08-29 11:13:22 +0100

    delete android demo

--- Android-Kotlin/example/Kotlin-for-Android-Developers-master/app/src/main/java/com/antonioleiva/weatherapp/extensions/ContextExtensions.kt
+++
@@ -1,6 +0,0 @@
-package com.antonioleiva.weatherapp.extensions
-
-import android.content.Context
-import android.support.v4.content.ContextCompat
-
-fun Context.color(res: Int): Int = ContextCompat.getColor(this, res)
--- android/example/Kotlin-for-Android-Developers-master/app/src/main/java/com/antonioleiva/weatherapp/domain/commands/Command.kt
+++
@@ -1,5 +0,0 @@
-package com.antonioleiva.weatherapp.domain.commands
-
-interface Command<out T> {
-    fun execute(): T
-}
