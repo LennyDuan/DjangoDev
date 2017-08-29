1eda890819c405ee25ff43d428ccc815020b6313
LennyDuan <hod8@aber.ac.uk>
2017-08-29 11:10:10 +0100

    delete demo

--- djangoRESTFramework/rest/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Lenny/Desktop/Final_Prject/code/djangoRESTFramework/rest/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
