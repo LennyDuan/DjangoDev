bb03148dd3871be748cf218ca67dc38704d4a52b
LennyDuan <hod8@aber.ac.uk>
2017-08-31 11:43:47 +0100

    delete unused api

--- myStudy/mystudy/polls/urls.py
+++ myStudy/mystudy/polls/urls.py
@@ -8,7 +8,6 @@ app_name = 'polls'
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-
     ## Restful API GET/POST/DELETE
 
 ################# DELETE API ##############
@@ -82,15 +81,4 @@ urlpatterns = [
     url(r'^api/v1/question/$', question.questionAll, name='question_all'),
     url(r'^api/v1/question/(?P<pk_id>[0-9]+)/$', question.questionDetail, name='question_detail'),
     url(r'^api/v1/question/(?P<study_field>.+)/field$', question.questionDetailByField, name='question_detail_field_list'),
-
-    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-
-    # url(r'^$', views.IndexView.as_view(), name='index'),
-    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
