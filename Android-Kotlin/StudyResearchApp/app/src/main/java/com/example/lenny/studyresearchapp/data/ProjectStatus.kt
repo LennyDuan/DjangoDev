e0a2a53601169294926957a03080a7c9ffb78e69
LennyDuan <hod8@aber.ac.uk>
2017-09-26 10:50:16 +0100

    modift text

--- Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/data/ProjectStatus.kt
+++ Android-Kotlin/StudyResearchApp/app/src/main/java/com/example/lenny/studyresearchapp/data/ProjectStatus.kt
@@ -6,12 +6,12 @@ package com.example.lenny.studyresearchapp.data
 
 enum class ProjectStatus constructor(val guide : String) {
     NONE("Setting up the system..."),
-    INIT("Hi kid!!! Would you like tell me about you?"),
-    ACCOUNT_DONE("Hi kid, Wow you are a good child! Would you mind answering these questions?"),
-    PRE_QUESTIONNAIRE("Hey, Don't For your questionnaire!!"),
-    PRE_QUESTIONNAIRE_Done("Good Job, Lets' start writing the story!!!"),
-    DIARY("Wow, do you want to tell me more about your story?"),
-    DIARY_DONE("Good stories!!! Questionnaire time is comming !!!"),
-    AFTER_QUESTIONNAIRE("Hey, Don't For last job. Good start with Good end!!"),
-    AFTER_QUESTIONNAIRE_DONE("Thanks Kid, your helps change the world!!!")
+    INIT("Please tell me about you?"),
+    ACCOUNT_DONE("Hi, there are really important information! Would you mind answering these questions?"),
+    PRE_QUESTIONNAIRE("Please don't Forget your questionnaire!"),
+    PRE_QUESTIONNAIRE_Done("Well done! Lets' start writing the diary stories!"),
+    DIARY("Please keep recording your story until the end date?"),
+    DIARY_DONE("Good stories! Same questionnaire is coming again!"),
+    AFTER_QUESTIONNAIRE("Please finish the last job. Good start with Good end!"),
+    AFTER_QUESTIONNAIRE_DONE("Thanks, your work helps a lot!")
 }
